420eb6ecbb6726db0c97e941908adf656404e7a9
Lucas Ahrens <lucahrens@icloud.com>
2019-02-07 21:24:06 -0500

    Create MyProgram.java

---
+++ MyProgram.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.util.Scanner;
+
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+         Hangman hangman = new Hangman();
+        hangman.playGame();
+    }
+}
